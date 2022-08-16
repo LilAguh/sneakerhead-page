8b73057c6c9e9274466914dffb415acac7a9e0f9
LilAguh <aguezeochoa@gmail.com>
2022-08-16 19:19:41 -0300

    add script.js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,39 @@
-let url = 'https://snkr-findr.herokuapp.com/sneakers/'
-let sneakers
-let cardContainer = document.getElementById('cardContainer')
+// let url = 'https://snkr-findr.herokuapp.com/sneakers/'
+// let sneakers
+// let cardContainer = document.getElementById('cardContainer')
 
-// fetch("./data.json")
-//     .then((respuesta) => respuesta.json())
-//     .then((datos) => sneakers = datos)
-//     .catch((error) => console.log(error))
+// // fetch("./data.json")
+// //     .then((respuesta) => respuesta.json())
+// //     .then((datos) => sneakers = datos)
+// //     .catch((error) => console.log(error))
 
-function printSneakers(sneakersToShow, container) {
-    container.innerHTML = ''
-    sneakersToShow.forEach(sneaker => {
-        let card = document.createElement('div')
-        card.className = `card`
-        card.innerHTML =
-            `
-        <div class="datos">
-        <img class="fotos" src=${sneaker.image_url}>
-        </div>
-        `
-        container.appendChild(card)
-    })
-}
+// function printSneakers(sneakersToShow, container) {
+//     container.innerHTML = ''
+//     sneakersToShow.forEach(sneaker => {
+//         let card = document.createElement('div')
+//         card.className = `card`
+//         card.innerHTML =
+//             `
+//         <div class="datos">
+//         <img class="fotos" src=${sneaker.image_url}>
+//         </div>
+//         `
+//         container.appendChild(card)
+//     })
+// }
 
-function fetchSneakers(URL, container) {
-    fetch(URL)
-        .then((res) => res.json())
-        .then((data) => {
-            printSneakers(data, container);
-        });
-}
+// function fetchSneakers(URL, container) {
+//     fetch(URL)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             printSneakers(data, container);
+//         });
+// }
 
-fetchSneakers("./data.json", cardContainer)
+// fetchSneakers(url, cardContainer)
+
+
+
+fetch("http://snkr-findr.herokuapp.com/sneakers/")
+    .then(res => res.json())
+    .then(data => console.log(data));
